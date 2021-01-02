ca3fd436cd24f8c5f11c36ffd48f7507c2ae40a2
cmunhozprog <cmunhozprog03@hotmail.com>
2021-01-02 20:42:28 -0300

    steep 14

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::get('/', HomeController::class)->name('home');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+
+Route::get('cursos', function(){
+    return "Aqui será a página de cursos";
+})->name('courses.index');
+
+Route::get('cursos/{course}', function($course){
+    return "Aqui será mostrada as insformações do curso";
+})->name('course.show');
